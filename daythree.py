c92e0e3bd5740b309d6d651dceded819d0fe592a
Hake <jonathanhake0@gmail.com>
2022-12-12 13:09:01 -0500

    hamburger

--- daythree.py
+++ daythree.py
@@ -8,8 +8,31 @@ def getVal(c):
 
 packs = []
 
-with open('daytwoinput.txt', encoding='utf-8') as f:
+with open('daythreeinput.txt', encoding='utf-8') as f:
     for line in f:
         line = line.strip('\n')
+        packs.append(line)
+        
     total = 0
     for pack in packs:
+        half = len(pack)//2
+        comp1 = pack[:half]
+        comp2 = pack[half:]
+        for item in comp1:
+            if item in comp2:
+                total += getVal(item)
+                break
+print(total)
+
+total = 0 
+length = len(packs)
+for i in range(0, length, 3):
+      pack1 = packs[i]
+      pack2 = packs[i + 1]
+      pack3 = packs[i + 2]
+      for item in pack1:
+          if item in pack2 and item in pack3:
+              total += getVal(item)
+              break
+          
+print(total)
